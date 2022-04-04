3d48d19873cecdd589ece85b7d0e68ee10436fca
我的二你模仿不来 <94788509+linbuer233@users.noreply.github.com>
2022-04-04 23:16:42 +0800

    2022/3/28

--- pythonhomework6.py
+++ pythonhomework6.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('rmm.74toRealtime.txt', header=None, skiprows=2, sep='\s+')
-varname = {'0': 'year', '1': 'month', '2': 'day', '3': 'RMM1', '4': 'RMM2', '5': 'phase', '6': 'amplitude',
-           '7': 'final_values'}
-df.rename(columns=varname)
+varname = {0: 'year', 1: 'month', 2: 'day', 3: 'RMM1', 4: 'RMM2', 5: 'phase', 6: 'amplitude', 7: 'final_values'}
+df = df.rename(columns=varname)
 df.to_csv('pythonhomework6csv.csv')
 df.to_excel('pythonhomework6excel.xlsx')
