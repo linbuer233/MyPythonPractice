4fb45f13739d09a244792c6095d099eb679aa3c3
我的二你模仿不来 <94788509+linbuer233@users.noreply.github.com>
2022-04-04 23:01:42 +0800

    2022/3/28

--- pythonhomework6.py
+++ pythonhomework6.py
@@ -1,35 +1,8 @@
 import pandas as pd
 
-with open("rmm.74toRealtime.txt", 'r') as f:
-    f.readline()
-    f.readline()
-    rmm = f.readlines()
-rmmall = []
-varname = ['year', 'month', 'day', 'RMM1', 'RMM2', 'phase', 'amplitude', 'final_values']
-for i in rmm:
-    p = 0
-    var_i = 0
-    dict = {}
-    count = 0
-    for j in i:
-        if j != ' ' and p == 0:
-            st = count
-            p = 1
-            count += 1
-            continue
-        if j == ' ' and p == 1:
-            end = count
-            dict[varname[var_i]] = i[st:end]
-            var_i += 1
-            count += 1
-            rmmall.append(dict)
-            p = 0
-            continue
-        if j == '\n':
-            dict[varname[var_i]] = i[st:count]
-            rmmall.append(dict)
-        count += 1
-
-df = pd.DataFrame.from_dict(rmmall[:])
+df = pd.read_csv('rmm.74toRealtime.txt', header=None, skiprows=2, sep='\s+')
+varname = {'0': 'year', '1': 'month', '2': 'day', '3': 'RMM1', '4': 'RMM2', '5': 'phase', '6': 'amplitude',
+           '7': 'final_values'}
+df.rename(columns=varname)
 df.to_csv('pythonhomework6csv.csv')
 df.to_excel('pythonhomework6excel.xlsx')
