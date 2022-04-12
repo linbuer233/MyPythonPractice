bb5d7a588a66f9e05c2beb7701648f4e395307da
我的二你模仿不来 <94788509+linbuer233@users.noreply.github.com>
2022-04-12 11:01:26 +0800

    2022/4/8

--- 小程序/lianWiFi.py
+++ 小程序/lianWiFi.py
@@ -7,29 +7,29 @@
 import os
 import time
 
-import requests
+## 导入selenium的浏览器驱动接口
 
 os.system('netsh wlan connect name=i-NUIST')
 time.sleep(2)
 print('连接中')
 
-data = {
-    "reauth": 'false',
-    "username": "your_phone_number",  #
-    "balance": "0.00",
-    "duration": "0",
-    "outport": "中国移动",  #
-    "totaltimespan": "0",
-    "usripadd": "10.0.204.91"
-}
-
-data1 = {
-    "username": "your_phone_number",  #
-    "password": "123789",  # 密码
-    "channel": "2",
-    "ifautologin": "0",  #
-    "pagesign": "secondauth",
-    "usripadd": "10.0.204.92"
-}
-r = requests.post('http://10.255.255.34/authentication/login', data=data)
-print('连接成功')
+# data = {
+#     "reauth": 'false',
+#     "username": "your_phone_number",  #
+#     "balance": "0.00",
+#     "duration": "0",
+#     "outport": "中国移动",  #
+#     "totaltimespan": "0",
+#     "usripadd": "10.0.204.91"
+# }
+#
+# data1 = {
+#     "username": "your_phone_number",  #
+#     "password": "123789",  # 密码
+#     "channel": "2",
+#     "ifautologin": "0",  #
+#     "pagesign": "secondauth",
+#     "usripadd": "10.0.204.92"
+# }
+# r = requests.post('http://10.255.255.34/authentication/login', data=data)
+# print('连接成功')
